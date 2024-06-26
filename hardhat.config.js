308fccc3e69106e0cb40f7cac6b6818a2e3fb41f
cdreamer1 <denisglushiko@gmail.com>
2024-06-26 07:12:11 -0600

    refactor: revise the config to resolve the verifying issue

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-
 module.exports = {
   solidity: {
     version: "0.8.20",
@@ -22,19 +21,38 @@ module.exports = {
       chainId: 1337,
     },
     base: {
-      url: `${process.env.ALCHEMY_BASE_URL}`,
+      url: process.env.ALCHEMY_BASE_URL,
       chainId: 8453,
       accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
-    }, 
-    sepolia: {
-      url: `${process.env.ALCHEMY_SEPOLIA_URL}`,
+    },
+    base_sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
       chainId: 84532,
       accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
     }, 
   }, 
   etherscan: {
     apiKey: {
-      baseSepolia: process.env.SEPOLIA_API_KEY,
+      "base": process.env.BASE_API_KEY,
+      "base-sepolia": process.env.SEPOLIA_API_KEY,
     },
+    customChains: [
+      {
+        network: "base",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org"
+        }
+      },
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org"
+        }
+      }
+    ]
   },
 };
